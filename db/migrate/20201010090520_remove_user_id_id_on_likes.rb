55c955d2cc5ee85f0232f169941154d716cde69a
xfm04x <66931014+xfm04x@users.noreply.github.com>
2020-10-12 11:12:38 +0900

    migrate修正

--- db/migrate/20201010090520_remove_user_id_id_on_likes.rb
+++
@@ -1,8 +0,0 @@
-class RemoveUserIdIdOnLikes < ActiveRecord::Migration[6.0]
-  def up
-    remove_column :likes, :user_id_id
-  end
-  def down
-    add_column :likes, :user_id_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_090520) do
+ActiveRecord::Schema.define(version: 2020_10_10_085219) do
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
