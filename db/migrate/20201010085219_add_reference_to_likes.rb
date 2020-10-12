b9125aedeb43d4ccbfa3cbb6eadf8282896df37c
xfm04x <66931014+xfm04x@users.noreply.github.com>
2020-10-12 12:11:11 +0900

    migration修正

--- db/migrate/20201010085219_add_reference_to_likes.rb
+++
@@ -1,8 +0,0 @@
-class AddReferenceToLikes < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :likes, :user_id
-    remove_column :likes, :term_id
-    add_reference :likes, :user, foreign_key: true
-    add_reference :likes, :term, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_085219) do
+ActiveRecord::Schema.define(version: 2020_10_05_092324) do
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "term_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
-    t.bigint "term_id"
     t.index ["term_id"], name: "index_likes_on_term_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
